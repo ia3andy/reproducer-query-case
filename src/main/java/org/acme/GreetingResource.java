0ae0180c4c4d75cd1df53a610d92182f6a31b5a8
Andy Damevin <ia3andy@gmail.com>
2023-02-10 09:07:27 +0100

    Add reproducer

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,16 +1,34 @@
 package org.acme;
 
+
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class GreetingResource {
 
     @GET
+    @Path("1")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from RESTEasy Reactive";
+    public String hello1(@QueryParam("S") @DefaultValue("empty") String S) {
+        return "Hello from RESTEasy Reactive " + S;
+    }
+
+    @GET
+    @Path("2")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello2(@QueryParam("s") @DefaultValue("empty")  String s) {
+        return "Hello from RESTEasy Reactive " + s;
+    }
+
+    @GET
+    @Path("3")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello2(@QueryParam("s") @DefaultValue("emptys")  String s, @QueryParam("S") @DefaultValue("emptyS")  String S) {
+        return "Hello from RESTEasy Reactive " + s  + " " + S;
     }
 }
--- src/test/java/org/acme/GreetingResourceTest.java
+++ src/test/java/org/acme/GreetingResourceTest.java
@@ -10,12 +10,60 @@ import static org.hamcrest.CoreMatchers.is;
 public class GreetingResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
+    public void testHello1Endpoint() {
         given()
-          .when().get("/hello")
+          .when().get("/hello/1?s=toto")
           .then()
              .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
+             .body(is("Hello from RESTEasy Reactive empty"));
+        given()
+            .when().get("/hello/1?S=toto")
+            .then()
+            .statusCode(200)
+            .body(is("Hello from RESTEasy Reactive toto"));
+        given()
+            .when().get("/hello/1?S=toto&s=bar")
+            .then()
+            .statusCode(200)
+            .body(is("Hello from RESTEasy Reactive toto"));
+    }
+
+    @Test
+    public void testHello2Endpoint() {
+        given()
+            .when().get("/hello/2?s=toto")
+            .then()
+            .statusCode(200)
+            .body(is("Hello from RESTEasy Reactive toto"));
+        given()
+            .when().get("/hello/2?s=toto&S=bar")
+            .then()
+            .statusCode(200)
+            .body(is("Hello from RESTEasy Reactive toto"));
+        given()
+            .when().get("/hello/2?S=toto")
+            .then()
+            .statusCode(200)
+            .body(is("Hello from RESTEasy Reactive empty"));
+    }
+
+    @Test
+    public void testHello3Endpoint() {
+        given()
+            .when().get("/hello/3?s=toto")
+            .then()
+            .statusCode(200)
+            .body(is("Hello from RESTEasy Reactive toto emptyS"));
+        given()
+            .when().get("/hello/3?s=toto&S=bar")
+            .then()
+            .statusCode(200)
+            .body(is("Hello from RESTEasy Reactive toto bar"));
+        given()
+            .when().get("/hello/3?S=toto")
+            .then()
+            .statusCode(200)
+            .body(is("Hello from RESTEasy Reactive emptys toto"));
     }
 
 }
